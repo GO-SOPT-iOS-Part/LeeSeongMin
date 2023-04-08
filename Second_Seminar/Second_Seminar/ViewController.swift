b36f86c4656963963390d47d0ff7a7d8d4cce823
seongmin221 <seongmin221@naver.com>
2023-04-08 10:17:48 +0900

    second seminar initial setting

---
+++ Second_Seminar/Second_Seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Second_Seminar
+//
+//  Created by 이성민 on 2023/04/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
