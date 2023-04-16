4bca2bce6e55b0218b1734ce6d281c8eb6637d0e
seongmin221 <seongmin221@naver.com>
2023-04-16 19:02:18 +0900

    [ADD] created views for custom bottom sheet

---
+++ Assingment_Tving/Assingment_Tving/Global/UIComponents/CustomBottomSheet/BottomSheetView.swift
@@ -0,0 +1,12 @@
+//
+//  BottomSheetView.swift
+//  Assingment_Tving
+//
+//  Created by 이성민 on 2023/04/15.
+//
+
+import UIKit
+
+final class BottomSheetView: UIView {
+    
+}
---
+++ Assingment_Tving/Assingment_Tving/Global/UIComponents/CustomBottomSheet/PassThroughView.swift
@@ -0,0 +1,12 @@
+//
+//  PassThroughView.swift
+//  Assingment_Tving
+//
+//  Created by 이성민 on 2023/04/15.
+//
+
+import UIView
+
+final class PassThroughView: UIView {
+    
+}
