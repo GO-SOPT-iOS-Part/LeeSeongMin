fac6b00d041ec5943c917e2d2e9bc27c8089b99c
seongmin221 <seongmin221@naver.com>
2023-04-17 22:21:15 +0900

    [CHORE] passthroughview in progress

--- Assingment_Tving/Assingment_Tving/Global/UIComponents/CustomBottomSheet/PassThroughView.swift
+++ Assingment_Tving/Assingment_Tving/Global/UIComponents/CustomBottomSheet/PassThroughView.swift
@@ -5,8 +5,11 @@
 //  Created by 이성민 on 2023/04/15.
 //
 
-import UIView
+import UIKit
 
 final class PassThroughView: UIView {
-    
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        let hitView = super.hitTest(point, with: event)
+        return hitView == self ? nil : hitView
+    }
 }
