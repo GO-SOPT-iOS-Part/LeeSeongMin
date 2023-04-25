80c3571bb24597bd824a526b021f80cc3cbd19d6
seongmin221 <seongmin221@naver.com>
2023-04-26 00:52:26 +0900

    [ADD] mypage view & VC

---
+++ Assingment_Tving/Assingment_Tving/Screen/View/MyPage/MyPageView.swift
@@ -0,0 +1,26 @@
+//
+//  MyPageView.swift
+//  Assingment_Tving
+//
+//  Created by 이성민 on 2023/04/26.
+//
+
+import UIKit
+
+import SnapKit
+
+final class MyPageView: BaseView {
+    
+    // MARK: - properties
+    
+    
+    
+    // MARK: - init
+    
+    
+    
+    // MARK: -  set
+    
+    
+
+}
---
+++ Assingment_Tving/Assingment_Tving/Screen/ViewController/MyPage/MyPageViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MyPageViewController.swift
+//  Assingment_Tving
+//
+//  Created by 이성민 on 2023/04/26.
+//
+
+import UIKit
+
+final class MyPageViewController: BaseViewController {
+    
+    // MARK: - properties
+    
+    
+    
+    // MARK: - life cycles
+    
+    
+    
+    // MARK: - set
+    
+    
+    
+    // MARK: - functions
+    
+    
+    
+    // MARK: - objc functions
+    
+
+}
