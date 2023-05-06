6f5b1d3c3588273269e2a2cc67be0f449d6338af
seongmin221 <seongmin221@naver.com>
2023-05-06 14:04:47 +0900

    [SETTING] initial setting

---
+++ Fourth_Seminar/Fourth_Seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fourth_Seminar
+//
+//  Created by 이성민 on 2023/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
