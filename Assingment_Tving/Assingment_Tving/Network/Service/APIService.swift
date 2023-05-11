5012b2f159e3e0ecda0ac448b8aabaafb4d048b9
seongmin221 <seongmin221@naver.com>
2023-05-12 02:11:07 +0900

    [CHORE] 자잘한 코드 수정

--- Assingment_Tving/Assingment_Tving/Network/Service/APIService.swift
+++ Assingment_Tving/Assingment_Tving/Network/Service/APIService.swift
@@ -56,16 +56,16 @@ extension APIService {
     }
     
     // api call with body
-    func callAPI(of endpoint: any RequestProtocol, with body: [String: Any], completion: @escaping NetworkClosure) {
-        let request = AF.request(Config.baseUrl + endpoint.path,
-                                 method: endpoint.method,
+    func callAPI(of api: any RequestProtocol & ResponseProtocol, with body: [String: Any], completion: @escaping NetworkClosure) {
+        let request = AF.request(Config.baseUrl + api.path,
+                                 method: api.method,
                                  parameters: body,
                                  encoding: JSONEncoding.default,
-                                 headers: endpoint.headers)
+                                 headers: api.headers)
         request.responseData { response in
             switch response.result {
             case .success:
-                completion(self.analyzeResponse(response, of: MoviePopularResponse.self))
+                completion(self.analyzeResponse(response, of: api.type))
             case .failure:
                 completion(.networkErr)
             }
@@ -73,35 +73,3 @@ extension APIService {
     }
     
 }
-
-//dataRequest.responseData { response in
-//    switch response.result {
-//    case .success:
-//        guard let statusCode = response.response?.statusCode else { return }
-//        guard let value = response.value else { return }
-//        let networkResult = self.judgeStatus(by: statusCode, value)
-//        completion(networkResult)
-//
-//    case .failure:
-//        completion(.networkErr)
-//    }
-//}
-//}
-//
-//private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
-//switch statusCode {
-//case 201: return isValidData(data: data)
-//case 400, 409: return isValidData(data: data)
-//case 500: return .serverErr
-//default: return .networkErr
-//}
-//}
-//
-//private func isValidData(data: Data) -> NetworkResult<Any> {
-//let decoder = JSONDecoder()
-//
-//guard let decodedData = try? decoder.decode(SignUpResponse.self, from: data) else { return .pathErr }
-//
-//return .success(decodedData as Any)
-//}
-//}
